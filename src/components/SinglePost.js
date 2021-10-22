120b79efaf200f07b29b10eac2275e8bbadb053d
sigvant <bnsviv@gmail.com>
2021-10-22 08:50:11 -0300

    before sanity url builder

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function SinglePost() {
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
+
   return (
     <div>
       <h1>Singlepost!</h1>
